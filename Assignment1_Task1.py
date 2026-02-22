e487be88758d46880e2923bcfaa0e43e1414928c
Raj-anu <anumeha.trainer@gmail.com>
2026-02-22 09:24:38 +0530

    Assignment 1 and 2

---
+++ Assignment1_Task1.py
@@ -0,0 +1,12 @@
+#Task 1: Perform Basic Mathematical Operations
+Num1 =int(input("Enter the first number:"))
+Num2 =int(input("Enter the second number:"))
+Addition = Num1 + Num2
+Substraction= Num1 -Num2
+Multiplication= Num1 * Num2
+Division= float(Num1/Num2)
+print("Addition:", Addition)
+print("Substraction:", Substraction)
+print("Multiplication:", Multiplication)
+print("Division:", Division)
+
